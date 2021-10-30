75c90b87e6f9c8e8d691b065a48277a16d055134
Francisco Rivas <85254638+Franriv88@users.noreply.github.com>
2021-10-29 21:53:26 -0300

    modficación pg Bienvenida
    arreglando la pagina de bienvenida
    Falta terminar varios detalles visuales

--- pantallaBienvenida.php
+++ pantallaBienvenida.php
@@ -36,16 +36,20 @@
 </head>
 <body>
     <h1>BIENVENDIDO! <?php echo $_SESSION["usuario"] ?> </h1>
-    <p class="p-bienvenida">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, aliquam est. Dolorem beatae praesentium alias numquam nisi error quaerat? Libero totam suscipit facilis, doloribus odit ducimus quasi maiores ab tempora!
-    Debitis ullam laudantium veniam culpa minus dignissimos, voluptate, ea quae natus rerum explicabo? Doloribus illum voluptatum quis accusantium dolores harum, dicta nobis fugiat aliquam inventore dolore facere, ut autem atque.
-    Aperiam quos suscipit, exercitationem velit ad maxime eius esse debitis eveniet nam non autem tenetur corrupti laudantium asperiores alias facilis quidem nostrum omnis porro sapiente obcaecati. Delectus vero ducimus pariatur?
-    Commodi magni debitis nostrum. Sequi, labore ullam? Nobis quaerat sint molestias! Eligendi sequi cum similique soluta, enim velit adipisci, facere dolorem perspiciatis libero, quo eius? Ducimus debitis similique ea ab!</p>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid pariatur reiciendis magni repudiandae numquam eos ipsam aspernatur eum, possimus officia aperiam illum cum sit, vitae fuga corporis, ducimus ad cupiditate.
-    Aspernatur, distinctio accusamus quis odio mollitia vel illo quibusdam? Molestiae quis voluptates incidunt. Est, dolor numquam aspernatur, vel eos, commodi cupiditate nesciunt ea impedit illum veniam qui. Consequatur, repudiandae neque.
-    Placeat aspernatur odio praesentium repellat? Repellendus neque placeat labore error soluta odio eligendi necessitatibus doloribus magni debitis, praesentium architecto eveniet, cumque delectus maxime ratione saepe asperiores consequuntur dicta aliquid totam?
-    Labore necessitatibus voluptatum temporibus recusandae quisquam ipsa magni earum laboriosam enim eligendi! Fugit perferendis molestias explicabo maxime sunt ratione, harum tenetur ipsa reprehenderit ex cumque quis! Reiciendis ipsum quas at?
-    Reiciendis fugiat animi cupiditate consequatur temporibus veniam suscipit sint doloremque id dolor, assumenda similique, eius quisquam repellendus illum modi? Deserunt omnis aspernatur in expedita doloremque eius laborum similique eaque ea.</p>
-<a href="cerrar.php">Carrar sesión</a>
+    <div class="p-bienvenida">
+        <p >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, aliquam est. Dolorem beatae praesentium alias numquam nisi error quaerat? Libero totam suscipit facilis, doloribus odit ducimus quasi maiores ab tempora!
+        Debitis ullam laudantium veniam culpa minus dignissimos, voluptate, ea quae natus rerum explicabo? Doloribus illum voluptatum quis accusantium dolores harum, dicta nobis fugiat aliquam inventore dolore facere, ut autem atque.
+        Aperiam quos suscipit, exercitationem velit ad maxime eius esse debitis eveniet nam non autem tenetur corrupti laudantium asperiores alias facilis quidem nostrum omnis porro sapiente obcaecati. Delectus vero ducimus pariatur?
+        Commodi magni debitis nostrum. Sequi, labore ullam? Nobis quaerat sint molestias! Eligendi sequi cum similique soluta, enim velit adipisci, facere dolorem perspiciatis libero, quo eius? Ducimus debitis similique ea ab!</p>
+        <p>LOREM ipsum dolor sit amet consectetur, adipisicing elit. Aliquid pariatur reiciendis magni repudiandae numquam eos ipsam aspernatur eum, possimus officia aperiam illum cum sit, vitae fuga corporis, ducimus ad cupiditate.
+        Aspernatur, distinctio accusamus quis odio mollitia vel illo quibusdam? Molestiae quis voluptates incidunt. Est, dolor numquam aspernatur, vel eos, commodi cupiditate nesciunt ea impedit illum veniam qui. Consequatur, repudiandae neque.
+        Placeat aspernatur odio praesentium repellat? Repellendus neque placeat labore error soluta odio eligendi necessitatibus doloribus magni debitis, praesentium architecto eveniet, cumque delectus maxime ratione saepe asperiores consequuntur dicta aliquid totam?
+        Labore necessitatibus voluptatum temporibus recusandae quisquam ipsa magni earum laboriosam enim eligendi! Fugit perferendis molestias explicabo maxime sunt ratione, harum tenetur ipsa reprehenderit ex cumque quis! Reiciendis ipsum quas at?
+        Reiciendis fugiat animi cupiditate consequatur temporibus veniam suscipit sint doloremque id dolor, assumenda similique, eius quisquam repellendus illum modi? Deserunt omnis aspernatur in expedita doloremque eius laborum similique eaque ea.</p>
+    </div >
+<div class="boton">
+    <a href="cerrar.php">Carrar sesión</a>
+</div>
 
 </body>
 </html>
