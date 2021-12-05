1cc3252a7e1899137a890755d2bb24b640ca9667
Francisco Rivas <85254638+Franriv88@users.noreply.github.com>
2021-12-05 15:44:17 -0300

    Udate crud.php

--- crud.php
+++ crud.php
@@ -1,16 +1,4 @@
 <?php
-  // CRUD = Creat, Read, Update, Delete
-  
-  //--- MySQL --- 
-  //Servidor = localhost
-  //Usuario = root
-  //Contraseña = " "
-  //Base de Datos = estudiantes
-
-  // array Asociativo: $datos_conexion = array("servidor" => "localhost", "usuario" => "root", "clave" => "", "basededatos" => "estudiantes");
-  //$concexion = mysqli_connect($datos_conexion["servidor], $datos_conexion["root], $datos_conexion["clave"], $datos_conexion["basededatos"] );
-  
-  //método aprendido del manual de PHP https://www.php.net/function.mysqli-connect
   $servername = "localhost";
   $username = "root";
   $password = "";
@@ -28,4 +16,23 @@
   // consulta a la BD
   $qery = "SELECT * FROM usuarios"; //esta no es una buena práctica
   var_dump($qery);
+
+
+
+
+
+  //------- comentarios y recordatorios --------
+  // CRUD = Creat, Read, Update, Delete
+  
+  //--- MySQL --- 
+  //Servidor = localhost
+  //Usuario = root
+  //Contraseña = " "
+  //Base de Datos = estudiantes
+
+  // array Asociativo: $datos_conexion = array("servidor" => "localhost", "usuario" => "root", "clave" => "", "basededatos" => "estudiantes");
+  //$concexion = mysqli_connect($datos_conexion["servidor], $datos_conexion["root], $datos_conexion["clave"], $datos_conexion["basededatos"] );
+  
+  //método aprendido del manual de PHP https://www.php.net/function.mysqli-connect
 ?>
+
